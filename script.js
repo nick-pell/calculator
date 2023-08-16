c3804f6bcc3e5954e225f77cbc8b9b9c9fdfa4e9
nick-pell <93624627+nick-pell@users.noreply.github.com>
2023-08-15 23:32:03 -0400

    Bug fix
    Fixed bug that prevented correct ids from being collected

--- script.js
+++ script.js
@@ -27,26 +27,45 @@ let secondNum;
 let operator;
 let displayValue;
 
+
 let display = document.querySelector('.display-container');
 let num = document.createElement('p');
 num.classList.add('display');
 num.textContent = displayValue;
 display.appendChild(num);
 
+const container = document.querySelector('.number-buttons');
+const buttons = container.querySelectorAll('button');
 
-const buttons = document.querySelectorAll('button');
+
 buttons.forEach((button) => {
     
     button.addEventListener('click', () => {
         num.textContent += button.textContent;
         display.appendChild(num);
         displayValue = num.textContent;
-        console.log(displayValue);
-
-
 
     });
 
 });
+
+const clear = document.querySelector('#clear');
+clear.addEventListener('click', () => {
+    num.textContent = "";
+    display.appendChild(num);
+    displayValue = "";
+});
+
+const opContainer = document.querySelector('.operation-buttons');
+const opButtons = opContainer.querySelectorAll('button');
+opButtons.forEach((opButton) => {
+    
+    opButton.addEventListener('click', () => {
+        operator = opButton.id;
+        num.textContent += opButton.textContent;
+        display.appendChild(num);
+    });
+});
+
 
 
